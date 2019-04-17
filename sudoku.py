c7e9b887cc1f0e89faaed3ed6eb6835792c2da12
pvahakan <pauli.vahakangas@gmail.com>
2019-04-17 21:42:18 +0300

    Removed useless random array creation function

--- sudoku.py
+++ sudoku.py
@@ -48,28 +48,6 @@ def console_print(ar):
         else:
             print()
 
-def create_random_sudoku():
-    """
-    Creates random 'sudoku like' type of 9x9 array for testing.
-
-    All rows has a numbers 1-9, columns and 3x3 squares are not checked.
-    """
-    n = 9
-    numbers = list(range(1, n+1))
-    row = np.array([])
-    array = np.zeros((n,n))
-
-    for i in range(n):
-        while len(row) < n:
-            r = random.choice(numbers)
-
-            if r not in row:  
-                row = np.append(row, r)
-
-        array[i] = row
-        row = []
-
-    return array
 
 def move(t, x1, y1, z1):
     cube[t[0]][t[1]][t[2]] += 1
